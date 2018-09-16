086c0601cfb6c7fb1a5ca04d763d5b7cb3cef6a8
Parolin <alessandro.parolin@sap.com>
2018-09-16 20:57:43 -0300

    Add additional canvas as buffer for performance

--- public/GridHelper.js
+++ public/GridHelper.js
@@ -6,6 +6,11 @@ export default class GridHelper{
     }
     this._canvas = canvas;
     this._ctx = this._canvas.getContext('2d');
+
+    this._bufferCanvas = document.createElement('canvas');
+    this._bufferCanvas.width = this._canvas.width;
+    this._bufferCanvas.height = this._canvas.height;
+    this._bufferCtx = this._bufferCanvas.getContext('2d');
   }
 
   drawGrid(rows, cols){
@@ -14,37 +19,44 @@ export default class GridHelper{
     this._rowHeight = this._canvas.height / this._rows;
     this._colWidth = this._canvas.width / this._cols;
 
+    this._bufferCtx.fillStyle = '#FFFFFF';
+    this._bufferCtx.fillRect(0, 0, this._bufferCanvas.width, this._bufferCanvas.height);
     this._drawRows();
     this._drawCols();
+    this.clearGrid();
+  }
+
+  clearGrid(){
+    this._ctx.drawImage(this._bufferCanvas, 0, 0);
   }
   
   _drawRows(color = '#777777'){
-    this._canvas.strokeStyle = color;
+    this._bufferCtx.strokeStyle = color;
     
     for (let row = 0; row < this._rows; row++){
       let start = {x: 0, y: row * this._rowHeight};
       let end = {x: this._canvas.width, y : row * this._rowHeight};
 
-      this._drawLine(start, end);
+      this._drawLine(this._bufferCtx, start, end);
     }
-    this._drawLine({x: 0, y: this._canvas.height}, {x: this._canvas.width, y: this._canvas.height});
+    this._drawLine(this._bufferCtx, {x: 0, y: this._canvas.height}, {x: this._canvas.width, y: this._canvas.height});
   }
   
   _drawCols(color = '#777777'){
-    this._canvas.strokeStyle = color;
+    this._bufferCtx.strokeStyle = color;
 
     for (let col = 0; col < this._cols; col++){
       let start = {x: col * this._colWidth, y: 0};
       let end = {x: col * this._colWidth, y: this._canvas.height}
-      this._drawLine(start, end);
+      this._drawLine(this._bufferCtx, start, end);
     }
-    this._drawLine({x: this._canvas.width, y: 0}, {x:this._canvas.width, y: this._canvas.height});
+    this._drawLine(this._bufferCtx, {x: this._canvas.width, y: 0}, {x:this._canvas.width, y: this._canvas.height});
   }
   
-  _drawLine(start, end){
-    this._ctx.moveTo(start.x, start.y);
-    this._ctx.lineTo(end.x, end.y);
-    this._ctx.stroke();
+  _drawLine(ctx, start, end){
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.stroke();
   }
 
   mouseCoord2gridCoord(x, y){
@@ -54,5 +66,17 @@ export default class GridHelper{
       row: Math.floor(row), 
       col: Math.floor(col)
     };
+  }
+
+  highlightCell(cell, color='#5c88ce'){
+    this.clearGrid();
+
+    const startX = cell.col * this._colWidth;
+    const endX = startX + this._colWidth;
+    const startY = cell.row * this._rowHeight;
+    const endY = startY + this._rowHeight;
+
+    this._ctx.fillStyle = color;
+    this._ctx.fillRect(startX, startY, this._colWidth, this._rowHeight);  
   }
 }
--- public/game.js
+++ public/game.js
@@ -2,13 +2,18 @@ import GridHelper from './GridHelper.js'
 
 const canvas = document.getElementById('canvas');
 
-canvas.addEventListener('click', (event) => {
-  const rect = canvas.getBoundingClientRect();
-  const mouseX = event.clientX - rect.left;
-  const mouseY = event.clientY - rect.top;  
+let lastVisitedCell = null;
 
-  console.log(grid.mouseCoord2gridCoord(mouseX, mouseY));
-});
+['mousemove','click'].forEach(eventName => {
+  canvas.addEventListener(eventName, (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;  
+  
+    const cell = grid.mouseCoord2gridCoord(mouseX, mouseY);
+    grid.highlightCell(cell);
+  });
+})
 
 const grid = new GridHelper(canvas);
 grid.drawGrid(5,5);
