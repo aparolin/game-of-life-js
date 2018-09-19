7dcd55228a9ac4dc718b687696c102968ec91a75
Parolin <alessandro.parolin@sap.com>
2018-09-19 20:17:53 -0300

    fix clock when pausing/resuming

--- public/Clock.js
+++ public/Clock.js
@@ -4,17 +4,21 @@ export default class Clock {
     this._state = 'stopped';
 
     this._requestId = null;
+    this._lastConsideredDate = null;
+    this._timeElapsedMs = 0;
   }
 
   start() {
     this._state = 'running';
-    this._gameStartedAt = new Date();
+    this._lastConsideredDate = new Date();
     this._update();
   }
 
   _update() {
-    const msElapsed = new Date() - this._gameStartedAt;
-    this._setClockText(msElapsed);
+    const delta = new Date() - this._lastConsideredDate;
+    this._timeElapsedMs += delta;
+    this._setClockText(this._timeElapsedMs);
+    this._lastConsideredDate = new Date();
 
     if (this._state === 'running') {
       this._requestId = requestAnimationFrame(this._update.bind(this));
@@ -29,6 +33,7 @@ export default class Clock {
   stop() {
     cancelAnimationFrame(this._requestId);
     this._state = 'stopped';
+    this._timeElapsedMs = 0;
 
     //send this to the end of the eventloop queue to ensure the animation
     //frame request really got cancelled
@@ -40,6 +45,9 @@ export default class Clock {
   }
 
   resume() {
+    this._lastConsideredDate = new Date();
+    this._state = 'running';
+
     if (this._state === 'stopped') {
       this.start();
     }
