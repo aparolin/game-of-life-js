4aac6e409aa9e0b5903d99e532e56c3e9914a54e
Parolin <alessandro.parolin@sap.com>
2018-09-19 16:53:52 -0300

    remove useless endpoint

--- app.js
+++ app.js
@@ -8,7 +8,3 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
 });
-
-app.get('/hello', (req, res) => {
-  res.status(200).send('<html><body><h1>All good!</h1></body></html>');
-})
